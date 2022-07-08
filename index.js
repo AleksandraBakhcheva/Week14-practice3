415623812a2e3697cfb7d90086d8eb56c1542f9d
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-07-08 12:47:57 +0500

    js update

--- index.js
+++ index.js
@@ -5,25 +5,39 @@ function previousPic() {
   document.querySelector('.first-pic').src="images/img4.jpeg";
 } */
 
-// с использованием if, все равно не работает
-/* function nextPic() {
+// с использованием условий if
+function nextPic() {
   let image = document.querySelector('.first-pic');
   if (image.src.match("images/img1.jpeg")) {
     image.src="images/img2.jpeg";  
   }
-  if (image.src.match("images/img2.jpeg")) {
+  else if (image.src.match("images/img2.jpeg")) {
     image.src="images/img3.jpeg"; 
   }
-  if (image.src.match("images/img3.jpeg")) {
+  else if (image.src.match("images/img3.jpeg")) {
     image.src="images/img4.jpeg"; 
   }
   else {
     image.src = "images/img1.jpeg";
   }
-} */
-
-// с циклом, работает идеально
-let slideIndex = 1;
+}
+function previousPic() {
+  let image = document.querySelector('.first-pic');
+  if (image.src.match("images/img1.jpeg")) {
+    image.src="images/img4.jpeg";  
+  }
+  else if (image.src.match("images/img4.jpeg")) {
+    image.src="images/img3.jpeg"; 
+  }
+  else if (image.src.match("images/img3.jpeg")) {
+    image.src="images/img2.jpeg"; 
+  }
+  else {
+    image.src = "images/img1.jpeg";
+  }
+}
+// с циклом
+/* let slideIndex = 1;
 
 function plusSlides(n) { 
   showSlides(slideIndex += n); 
@@ -38,4 +52,4 @@ function showSlides(n) {
       slides[i].style.display = "none"; 
   }
   slides[slideIndex-1].style.display = "block";   
-}
+} */
